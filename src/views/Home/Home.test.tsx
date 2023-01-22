6a9cbdaa4f87989e8639a002cd5b9580a6c65f78
ilovric00 <ilovric@profico.hr>
2023-01-22 15:16:04 +0100

    Home test updated

--- src/views/Home/Home.test.tsx
+++ src/views/Home/Home.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import Home from './Home';
 
 test('renders go to task link', () => {
-  render(<Home />);
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
 
   const linkElement = screen.getByText(/go to task/i);
 
