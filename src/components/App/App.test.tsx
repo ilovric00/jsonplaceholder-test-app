6aa830e271ad06c87e9697407f9e1750a2d531a9
ilovric00 <ilovric@profico.hr>
2023-01-22 01:34:23 +0100

    Update tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders go to task link', () => {
   render(<App />);
 
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/go to task/i);
 
   expect(linkElement).toBeInTheDocument();
 });
